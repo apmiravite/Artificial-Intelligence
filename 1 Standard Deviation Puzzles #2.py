ffdfdfc85cba0a4a65841ccec379287a95c4dc51
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-18 12:58:57 +0800

    Create 1 Standard Deviation Puzzles #2.py

---
+++ 1 Standard Deviation Puzzles #2.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/standard-deviation-puzzles-2/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+mu = 0.675
+sigma = 0.065
+X=0.9025
+
+print((X-mu)/sigma)
