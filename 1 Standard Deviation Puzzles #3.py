87a75517f34e3c52c35de10f97ea21e8759baaa2
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-20 11:58:03 +0800

    Create 1 Standard Deviation Puzzles #3.py

---
+++ 1 Standard Deviation Puzzles #3.py
@@ -0,0 +1,6 @@
+#https://www.hackerrank.com/challenges/standard-deviation-puzzles-3/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+sigma1=3
+sigma2=4
+print(format((sigma1**2+sigma2**2)**0.5, "0.1f"))
