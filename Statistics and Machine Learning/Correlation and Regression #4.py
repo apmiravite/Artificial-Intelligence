fa21829f6a21b2dac072c663eaa79e051d4b7f1d
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-21 19:02:13 +0800

    Create Correlation and Regression #4.py

---
+++ Statistics and Machine Learning/Correlation and Regression #4.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/correlation-and-regression-lines-4/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+y=7
+b=107
+val_x=(9*y+b)*(1/20)
+print(round(val_x,1))
