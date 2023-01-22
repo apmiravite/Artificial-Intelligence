710b3e91e8bbe25e97a0c22e895c6dd0e12e32fc
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-23 00:56:54 +0800

    Create Compute the cross-entropy.py

---
+++ Natural Language Processing/Compute the cross-entropy.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/nlp-compute-the-cross-entropy/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+# perplexity = 2^x (solve for x)
+
+print(round(math.log(170)/math.log(2), 2))
