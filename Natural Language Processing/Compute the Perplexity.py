ae63e381a8474044e40ecd1082b494acc3c9c152
AP Miravite <87432826+apmiravite@users.noreply.github.com>
2023-01-23 00:59:19 +0800

    Create Compute the Perplexity.py

---
+++ Natural Language Processing/Compute the Perplexity.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/nlp-compute-the-perplexity/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# x= 2^(cross-entropy) [solve for x]
+
+print(round(2**9.91,0))
